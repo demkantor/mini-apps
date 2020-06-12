b4086bb2ab19dcccea601b9993e01200e14af28e
demkantor <demkantor@gmail.com>
2020-06-12 18:15:42 -0500

    added scroll demo project

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import './App.css';
-import Nav from './Nav/Nav';
 import Home from './Home/Home';
+import Nav from './Nav/Nav';
+
+
 import ColorFlip from './ColorFlip/ColorFlip';
 import Counter from './Counter/Counter';
-import Reviews from './Reviews/Reviews';
-import NavExample from './NavExample/NavExample';
-import SidebarExample from './SidebarExample/SidebarExample';
-import Modal from './Modal/Modal';
-import Questions from './Questions/Questions';
 import Menu from './Menu/Menu';
+import Modal from './Modal/Modal';
+import NavExample from './NavExample/NavExample';
+import Questions from './Questions/Questions';
+import Reviews from './Reviews/Reviews';
+import Scroll from './Scroll/Scroll';
+import SidebarExample from './SidebarExample/SidebarExample';
 import Video from './Video/Video';
 
 
@@ -24,14 +27,16 @@ class App extends Component {
           <div className="main-wrapper">
             <Switch>
               <Route exact path="/" component={Home}/>
+
               <Route exact path="/color-flipper" component={ColorFlip}/>
               <Route exact path="/counter" component={Counter}/>
-              <Route exact path="/reviews" component={Reviews}/>
-              <Route exact path="/navbar" component={NavExample}/>
-              <Route exact path="/sidebar" component={SidebarExample}/>
-              <Route exact path="/modal" component={Modal}/>
-              <Route exact path="/questions" component={Questions}/>
               <Route exact path="/menu" component={Menu}/>
+              <Route exact path="/modal" component={Modal}/>
+              <Route exact path="/navbar" component={NavExample}/>
+              <Route exact path="/questions" component={Questions}/>
+              <Route exact path="/reviews" component={Reviews}/>
+              <Route exact path="/scroll" component={Scroll}/>
+              <Route exact path="/sidebar" component={SidebarExample}/>
               <Route exact path="/video" component={Video}/>
 
               {/* 404 page */}
---
+++ src/components/Scroll/Scroll.js
@@ -0,0 +1,126 @@
+import React, { Component } from 'react';
+import './Scroll.css';
+
+class Scroll extends Component {
+
+    state = {
+        year: 2020
+    };
+
+    componentDidMount = () => {
+        // set current year in footer
+        const date = new Date().getFullYear();
+        this.setState({ year: date });
+
+        // set navbar to dynamically turn fixed upon scrolling past its height
+        const navbar = document.getElementById("nav");
+        const topLink = document.querySelector(".top-link");
+        window.addEventListener(("scroll"), () => {
+            const scrollHeight = window.pageYOffset;
+            const navHeight = navbar.getBoundingClientRect().height;
+            if (scrollHeight > navHeight) {
+                navbar.classList.add("fixed-nav");
+            } else {
+                navbar.classList.remove("fixed-nav");
+            };
+            // add back to home button after 500px scroll
+            if (scrollHeight > 500) {
+                topLink.classList.add("scroll-show-link");
+            } else {
+                topLink.classList.remove("scroll-show-link");
+            };
+        });
+    };
+
+    toggleNav = () => {
+        const linksContainer = document.querySelector(".links-container");
+        linksContainer.classList.toggle('scroll-show-links');
+
+        // dynamically grow navbar size depending on number of links
+        // const links = document.querySelector(".scroll-links");
+        // const containerHeight = linksContainer.getBoundingClientRect();
+        // const linksHeight = links.getBoundingClientRect().height;
+        // if (containerHeight === 0) {
+        //     linksContainer.style.height = `${linksHeight}px`;
+        // } else {
+        //     linksContainer.style.height = 0;
+        // }
+    };
+
+
+    render() {
+        return (
+            <>
+                <header id="home"className="scroll-header">
+                    <nav id="nav" className="scroll-nav">
+                        <div className="scroll-nav-center">
+                            <div className="scroll-nav-header">
+                                <img src="images/logo2.svg" alt="logo" className="scroll-logo" />
+                                <button className="scroll-nav-toggle" onClick={this.toggleNav}>
+                                    <i className="fas fa-bars" />
+                                </button>
+                            </div>
+                            <div className="links-container">
+                                <ul className="scroll-links">
+                                    <li>
+                                        <a href="#home" className="scroll-link">home</a>
+                                    </li>
+                                    <li>
+                                        <a href="#about" className="scroll-link">about</a>
+                                    </li>
+                                    <li>
+                                        <a href="#services" className="scroll-link">services</a>
+                                    </li>
+                                    <li>
+                                        <a href="#tours" className="scroll-link">tours</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                    <div className="scroll-banner">
+                        <div className="scroll-container">
+                            <h1>Scroll Demo Project</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                                Est dolore illo dolores quia nemo doloribus quaerat, 
+                                magni numquam repellat reprehenderit.
+                            </p>
+                            <a href="#tours" className="scroll-link btn btn-white">
+                                explore tours
+                            </a>
+                        </div>
+                    </div>
+                </header>
+                <section id="about" className="section">
+                    <div className="scroll-title">
+                        <h2>about <span>us</span></h2>
+                    </div>
+                </section>
+                <section id="services" className="section">
+                    <div className="scroll-title">
+                        <h2>our <span>services</span></h2>
+                    </div>
+                </section>
+                <section id="tours" className="section">
+                    <div className="scroll-title">
+                        <h2>featured <span>tours</span></h2>
+                    </div>
+                </section>
+                <footer className="section">
+                    <p>
+                        copyright &copy; backroads travel tours company
+                        <span id="date">
+                            {" "}{this.state.year}
+                        </span>. all rights reserved
+                    </p>
+                </footer>
+                <a className="scroll-link top-link" href="#home">
+                    <i className="fas fa-arrow-up" />
+                </a>
+            </>
+        )
+    }
+};
+
+export default Scroll;
